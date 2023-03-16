14892814a4bfdb577854ae16e7037dc8e51f936f
Luis Angel Salgueron Prado <salgueronprado18@gmail.com>
2023-03-16 12:01:33 -0500

    implementacion de tabla para producto

--- view/MntProducto/index.php
+++ view/MntProducto/index.php
@@ -36,6 +36,9 @@
     <link href="../../public/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
     <link href="../../public/lib/jquery-switchbutton/jquery.switchButton.css" rel="stylesheet">
 
+    <link href="../../public/lib/datatables/jquery.dataTables.css" rel="stylesheet">
+    <link href="../../public/lib/select2/css/select2.min.css" rel="stylesheet">
+
     <!-- Bracket CSS -->
     <link rel="stylesheet" href="../../public/css/bracket.css">
   </head>
@@ -675,20 +678,36 @@
     <div class="br-mainpanel">
       <div class="br-pageheader pd-y-15 pd-l-20">
         <nav class="breadcrumb pd-0 mg-0 tx-12">
-          <a class="breadcrumb-item" href="index.html">Bracket</a>
-          <span class="breadcrumb-item active">Blank Page</span>
+          <a class="breadcrumb-item" href="index.html">Mantenimiento</a>
+          <span class="breadcrumb-item active">Producto</span>
         </nav>
       </div><!-- br-pageheader -->
       <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
-        <h4 class="tx-gray-800 mg-b-5">Blank Page (Default Layout)</h4>
-        <p class="mg-b-0">Introducing Bracket admin template, the most handsome admin template of all time.</p>
+        <h4 class="tx-gray-800 mg-b-5">Producto</h4>
+        <p class="mg-b-0">Desde esta ventana podra dar mantenimiento a productos</p>
       </div>
 
       <div class="br-pagebody">
 
-        <!-- start you own content here -->
+      <div class="br-section-wrapper">
+          <h6 class="tx-gray-800 tx-uppercase tx-bold tx-14 mg-b-10">Mantenimiento de Producto</h6>
+          <div class="table-wrapper">
+            <table id="producto_data" class="table display responsive nowrap">
+              <thead>
+                <tr>
+                  <th class="wd-15p">Nombre</th>
+                  <th class="wd-15p"></th>
+                  <th class="wd-20p"></th>
+                </tr>
+              </thead>
+              <tbody>
 
-      </div><!-- br-pagebody -->
+              </tbody>
+            </table>
+          </div>
+      </div>
+        
+    </div><!-- br-pagebody -->
 
     </div><!-- br-mainpanel -->
     <!-- ########## END: MAIN PANEL ########## -->
@@ -703,5 +722,17 @@
     <script src="../../public/lib/peity/jquery.peity.js"></script>
 
     <script src="../../public/js/bracket.js"></script>
+
+    <script src="../../public/lib/highlightjs/highlight.pack.js"></script>
+    <script src="../../public/lib/datatables/jquery.dataTables.js"></script>
+    <script src="../../public/lib/highlightjs/highlight.pack.js"></script>
+    <script src="../../public/lib/datatables-responsive/dataTables.responsive.js"></script>
+    <script src="../../public/lib/select2/js/select2.min.js"></script>
+
+
+
+
+
+    <script type="text/javaScript" src="mntproducto.js"></script>
   </body>
 </html>
